fce1bebfa13ff90d1e95c1058e86c9961f6115b5
phoebesu1025 <phoebesu85@yahoo.com.tw>
2024-06-03 22:00:39 +1200

    first commit

---
+++ pictureData.js
@@ -0,0 +1,93 @@
+// Dummy Data
+const pictureData = [
+    {
+      id: 1,
+      name: "Cotton Candy",
+      image:
+        "https://www.baskinrobbinsmea.com/wp-content/uploads/2020/05/Cotton-Candy.jpg",
+      flavour: "Rainbow",
+      dairyOption: "Non Dairy Free",
+      color: "Pink",
+    },
+    {
+      id: 2,
+      name: "Moddy Choco",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToNb4qLVc8li_M2NHBfEl14DaVpUCmplY5lg&usqp=CAU",
+      flavour: "Chocolate",
+      dairyOption: "Non Dairy Free",
+      color: "Chocolate",
+    },
+    {
+      id: 3,
+      name: "Popping Star",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8wsZrVmvqy6VL4k_cMj3V8fM4A4JiIRPkFw&usqp=CAU",
+      flavour: "Rainbow",
+      dairyOption: "Non Dairy Free",
+      color: "Others",
+    },
+    {
+      id: 4,
+      name: "Raspberry Passion",
+      image:
+        "https://th.bing.com/th/id/OIP.OqI71CUGz0o-fH3Pq0706gHaHa?w=184&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      flavour: "Fruit",
+      dairyOption: "Dairy Free",
+      color: "Pink",
+    },
+    {
+      id: 5,
+      name: "Choc Swirl",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhu_B4Po6ELToO3ASS8D6-PX8ZyLtwex8tzQ&usqp=CAU",
+      flavour: "Chocolate",
+      dairyOption: "Non Dairy Free",
+      color: "Chocolate",
+    },
+    {
+      id: 6,
+      name: "Nilly Nilla",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaCMea3WfNnyq71aUiRzVakN7PIyQPBZIeLg&usqp=CAU",
+      flavour: "Vanilla",
+      dairyOption: "Dairy Free",
+      color: "White",
+    },
+    {
+      id: 7,
+      name: "Rainbow Heaven",
+      image:
+        "https://www.epicuricloud.com/wp-content/uploads/2021/05/Vanilla-Ice-Cream-Cone-up-close-scaled.jpeg",
+      flavour: "Rainbow",
+      dairyOption: "Non Dairy Free",
+      color: "Others",
+    },
+    {
+      id: 8,
+      name: "Blueberry Icecream",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLJ2cP6fasme5jSgjAcFGriiSBcruoPjFI6Q&usqp=CAUg",
+      flavour: "Fruit",
+      dairyOption: "Dairy Free",
+      color: "Others",
+    },
+    {
+      id: 9,
+      name: "Cotton Candy",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZZLndYJXGyzwMnIIaZPrm2MUhoiutN3NCyA&usqp=CAU",
+      flavour: "Rainbow",
+      dairyOption: "Non Dairy Free",
+      color: "Pink",
+    },
+    {
+      id: 10,
+      name: "Wiskey Lover",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSep65gDA6RH8NsuxV4dIovWT4KPBAjfrMdig&usqp=CAU",
+      flavour: "Chocolate",
+      dairyOption: "Non Dairy Free",
+      color: "Chocolate",
+    },
+  ];
